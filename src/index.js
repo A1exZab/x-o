38121fa035d1c50cb321f2e9f8b187fe43278669
A1exZab <lisandrugrin@gmail.com>
2021-11-07 16:42:29 +0300

    Added jumpTo() in class Game

--- src/index.js
+++ src/index.js
@@ -84,7 +84,15 @@ class Game extends React.Component {
 					squares: squares,
 				},
 			]),
+			stepNumber: 0,
 			xIsNext: !this.state.xIsNext,
+		})
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
 		})
 	}
 
@@ -96,7 +104,7 @@ class Game extends React.Component {
 		const moves = history.map((step, move) => {
 			const desc = move ? 'Go to step #' + move : 'Restart game'
 			return (
-				<li>
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)}>{desc}</button>
 				</li>
 			)
